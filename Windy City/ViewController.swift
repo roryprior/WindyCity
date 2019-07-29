32715976a305a9456251471df69519d9ebc6aa8e
Rory Prior <rory@thinkmac.co.uk>
2019-07-29 14:12:09 +0100

    Initial Commit

---
+++ Windy City/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Windy City
+//
+//  Created by Rory Prior on 29/07/2019.
+//  Copyright © 2019 ThinkMac Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
