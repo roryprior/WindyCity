8d553bc5854ba547c413d89cbf1b8d8a083cc86d
Rory Prior <rory@thinkmac.co.uk>
2019-07-30 11:18:35 +0100

    Adds CocoaPods so we can easily import PureLayout to make programatic autolayout painless. Adds some image assets for our custom compass view to show wind direction graphically.

---
+++ Pods/Target Support Files/Pods-Windy City/Pods-Windy City-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Windy_City : NSObject
+@end
+@implementation PodsDummy_Pods_Windy_City
+@end
---
+++ Windy City/View/CompassView.swift
@@ -0,0 +1,50 @@
+//
+//  CompassView.swift
+//  Windy City
+//
+//  Created by Rory Prior on 30/07/2019.
+//  Copyright © 2019 ThinkMac Software. All rights reserved.
+//
+
+import UIKit
+import PureLayout
+
+class CompassView: UIView {
+
+  private var backgroundImageView = UIImageView.init()
+  private var arrowImageView = UIImageView.init()
+  
+  var degrees = 0 {
+    didSet {
+      let radians = CGFloat(Double(degrees) * (.pi / 180))
+      arrowImageView.transform = CGAffineTransform(rotationAngle: radians)
+    }
+  }
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setup()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    setup()
+  }
+  
+  func setup() {
+    
+    self.addSubview(backgroundImageView)
+    self.addSubview(arrowImageView)
+    
+    backgroundImageView.image = UIImage.init(named: "Compass")
+    arrowImageView.image = UIImage.init(named: "Arrow")
+    
+    backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+    arrowImageView.translatesAutoresizingMaskIntoConstraints = false
+    
+    backgroundImageView.autoPinEdgesToSuperviewEdges()
+    arrowImageView.autoPinEdgesToSuperviewEdges()
+  }
+  
+
+}
